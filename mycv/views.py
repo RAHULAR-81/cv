d5b361538f5280e338ce60bdaa4653ba2c237772
RAHULAR-81 <122857066+RAHULAR-81@users.noreply.github.com>
2023-01-23 14:25:39 +0530

    Delete mycv directory

--- mycv/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from . import models
-
-def cv(request):
-    lan = models.language.objects.all
-    skill = models.skills.objects.all
-    expert = models.expertise.objects.all
-    edu = models.education.objects.all
-    return render(request, 'cv.html', {'lan':lan,'skill':skill,'expert':expert,'edu':edu})
